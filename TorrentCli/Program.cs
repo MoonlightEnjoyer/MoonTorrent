c10832bda86c7116f589afe5483669ef6d34e94c
artiom.labietskii <artiom.labietskii@gmail.com>
2023-11-29 14:14:13 +0300

    Correct info hash calculation

--- TorrentCli/Program.cs
+++ TorrentCli/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using TorrentCore;
+
+TorrentFileParser parser = new TorrentFileParser();
+
+string infoHash = parser.CalculateInfoHash("F:\\MoonTorrent\\Red Hot Chili Peppers - Unlimited Love - 2022.torrent");
+
+Console.WriteLine(infoHash);
--- TorrentCore/TorrentFileParser.cs
+++ TorrentCore/TorrentFileParser.cs
@@ -7,25 +7,40 @@ namespace TorrentCore
     {
         public string CalculateInfoHash(string fileName)
         {
-            using StreamReader torrentFile = new StreamReader(fileName);
-            string fileContent = torrentFile.ReadToEnd();
-            int startPos = fileContent.IndexOf("info") + 4;
-            int endPos = 0;
- 
-            for(int i=3;i<fileContent.Length;i++)
+            using FileStream torrentFileStream = new FileStream(fileName, FileMode.Open);
+            using BinaryReader binaryReader = new BinaryReader(torrentFileStream);
+            var binaryContent = binaryReader.ReadBytes((int)torrentFileStream.Length);
+
+            int startPos = -1;
+
+            for (int i = 0; i < torrentFileStream.Length; i++)
             {
-                if(fileContent[fileContent.Length-i]=='e' && (fileContent[fileContent.Length-i+1]>='0' && fileContent[fileContent.Length-i+1]<='9'))
+                if (binaryContent[i] == 'i' && binaryContent[i + 1] == 'n' && binaryContent[i + 2] == 'f' && binaryContent[i + 3] == 'o')
                 {
-                    endPos=i;
+                    startPos = i + 4;
+                }
+            }
+
+            int endPos = 0;
+
+            for (int i = 3; i < torrentFileStream.Length; i++)
+            {
+                if (binaryContent[torrentFileStream.Length - i] == 'e' && (binaryContent[torrentFileStream.Length - i + 1] >= '0' && binaryContent[torrentFileStream.Length - i + 1] <= '9'))
+                {
+                    endPos = (int)(torrentFileStream.Length - i + 1);
                     break;
                 }
             }
-            
 
-            string infoValue = fileContent[startPos..endPos];
+            byte[] infoValue = binaryContent[startPos..endPos];
+            string res = "";
 
             using var sha1 = SHA1.Create();
-            return Convert.ToHexString(sha1.ComputeHash(Encoding.UTF8.GetBytes(infoValue)));
+            var hash = sha1.ComputeHash(infoValue);
+
+            res = Convert.ToHexString(hash);
+
+            return res;
         }
     }
 }
